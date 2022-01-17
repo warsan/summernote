9292e1e8f8ee9d3d19559a8675c881b25fa70f07
Сандро <warsan00@mail.ru>
2022-01-18 03:33:08 +0500

    правка

--- src/js/base/editing/Typing.js
+++ src/js/base/editing/Typing.js
@@ -84,8 +84,7 @@ export default class Typing {
           ) {
             $(rng.sc.nextSibling).remove();
           }
-          const split = dom.splitTree(blockquote, rng.getStartPoint(), {
-            isDiscardEmptySplits: true
+          const split = dom.splitTree(blockquote, rng.getStartPoint(), {  isDiscardEmptySplits: true,
           });
           if (split) {
             split.parentNode.insertBefore(nextPara, split);
